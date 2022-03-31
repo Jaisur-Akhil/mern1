6ab5c94c1fb705dcc196b866ff57e0d51412df54
Akhil <77776342+Jaisur-Akhil@users.noreply.github.com>
2022-04-01 00:10:12 +0530

    Avatar

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,25 +1,25 @@
 /** @format */
 
 import React from 'react';
+import Avatar from '../../shared/components/Avatar/Avatar';
 import './UserItem.css';
 
 const UserItem = (props) => {
   return (
-    <div>
-      <li className='user-item'>
-        <div className='user-item_content'>
-          <div className='user-item_image'>
-            <img src={props.image} alt={props.name} />
-          </div>
-          <div user-item_info>
-            <h2>{props.name}</h2>
-            <h2>
-              {props.placeCount} {props.placeCount === 1 ? ' Place' : ' Places'}
-            </h2>
-          </div>
+    <li className='user-item'>
+      <div className='user-item__content'>
+        <div className='user-item__image'>
+          {/* <img src={props.image} alt={props.name} />66664 */}
+          <Avatar />
         </div>
-      </li>
-    </div>
+        <div className='user-item__info'>
+          <h2>{props.name}</h2>
+          <h3>
+            {props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}
+          </h3>
+        </div>
+      </div>
+    </li>
   );
 };
 
--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -5,16 +5,16 @@ import './UsersList.css';
 import UserItem from './UserItem';
 
 const UsersList = (props) => {
-  //   if (props.items.length === 0) {
-  //     return (
-  //       <div>
-  //         <h2>No Users Found</h2>
-  //       </div>
-  //     );
-  //   }
+  if (props.items.length === 0) {
+    return (
+      <div className='center'>
+        <h2>No Users Found</h2>
+      </div>
+    );
+  }
 
   return (
-    <ul>
+    <ul className='users-list'>
       {/* /* {}//js expression */}
       {props.items.map((user) => {
         return (
