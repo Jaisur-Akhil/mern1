5d5228cc4cc06d2700ba25547caa72dde85bab17
Akhil <77776342+Jaisur-Akhil@users.noreply.github.com>
2022-04-01 11:27:48 +0530

    Side Drawer

--- src/shared/Navigation/MainNavigation.js
+++ src/shared/Navigation/MainNavigation.js
@@ -1,25 +1,33 @@
 /** @format */
 
-import React from 'react';
+import React, { Fragment } from 'react';
 import './MainNavigation.css';
 import MainHeader from './MainHeader';
 import { Link } from 'react-router-dom';
 import NavLinks from './NavLinks';
+import SideDrawer from './SideDrawer';
 const MainNavigation = (props) => {
   return (
-    <MainHeader>
-      <button className='main-navigation_menu-btn'>
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className='main-navigation_title'>
-        <Link to='/'>YourPlaces</Link>
-      </h1>
-      <nav className='main-navigation_header-nav'>
-        <NavLinks />
-      </nav>
-    </MainHeader>
+    <React.Fragment>
+      <SideDrawer>
+        <nav className='main-navigation_drawer-nav'>
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+      <MainHeader>
+        <button className='main-navigation_menu-btn'>
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className='main-navigation_title'>
+          <Link to='/'>YourPlaces</Link>
+        </h1>
+        <nav className='main-navigation_header-nav'>
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </React.Fragment>
   );
 };
 
---
+++ src/shared/Navigation/SideDrawer.js
@@ -0,0 +1,9 @@
+/** @format */
+
+import react from 'react';
+
+export const SideDrawer = (props) => {
+  return <aside className='side-drawer'>{props.children}</aside>;
+};
+
+export default SideDrawer;
